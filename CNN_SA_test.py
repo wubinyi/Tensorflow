c053a8b6a679458c4c1c0f5d52037bece6c05175
wubinyi <wbybinyiwu@gmail.com>
2018-02-18 10:51:34 +0100

    quantization model testcase generated for CNNA hardware

--- CNN_SA_test.py
+++ CNN_SA_test.py
@@ -1,3 +1,8 @@
+# Enter Tensorflow's install direction, hier is ~/tensorflow
+# use following command to quantizate the model
+# bazel-bin/tensorflow/tools/quantization/quantize_graph --input=/home/wubinyi/Machine_Learning_NN/Tensorflow/saved_model/cnn_sa_test.pb --output_node_name="Y1" --print_node --output=/home/wubinyi/Machine_Learning_NN/Tensorflow/quantization_model/cnn_sa_test_quantizate.pb --mode=eightbit
+
+
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 import tensorflow as tf
@@ -5,11 +10,23 @@ import math
 from tensorflow.examples.tutorials.mnist import input_data as mnist_data
 from tensorflow.python.framework import graph_util
 
-X = tf.placeholder(tf.float32, [None, 25], name="X")
-X = tf.reshape(X, shape=[-1, 5, 5, 1], name='reshape_X')
+# input  [batch, in_height, in_width, in_channels]
+# filter [filter_height, filter_width, in_channels, out_channels]
+filter_width = 3
+filter_height = filter_width
+feature_map_height = 10
+feature_map_width = 10
+input_channels = 16
+output_channels = 16
 
-W1 = tf.Variable(tf.truncated_normal([3, 3, 1, 2], stddev=0.1), name="W1")
-B1 = tf.Variable(tf.random_normal([2]), name="B1")
+# filename_num = str(filter_height)+"_"+str(filter_width)+"_"+str(feature_map_height)+"_"+str(feature_map_width)+"_"+str(input_channels)+"_"+str(output_channels)+"_"
+# filename = "saved_model/cnna_sa_"+filename_num+".pb"
+
+X = tf.placeholder(tf.float32, [None, feature_map_height*feature_map_width*input_channels], name="X")
+X = tf.reshape(X, shape=[-1, feature_map_height, feature_map_width, input_channels], name='reshape_X')
+
+W1 = tf.Variable(tf.truncated_normal([filter_height, filter_width, input_channels, output_channels], stddev=0.1), name="W1")
+B1 = tf.Variable(tf.random_normal([output_channels]), name="B1")
 
 
 stride = 1  # output is 24x24
@@ -22,5 +39,6 @@ init = tf.global_variables_initializer()
 with tf.Session() as sess:
 	sess.run(init)
 	output_graph_def = graph_util.convert_variables_to_constants(sess, sess.graph_def, ['Y1'])
-	with tf.gfile.GFile("saved_model/cnn_sa_test.pb", "wb") as f:
+	with tf.gfile.GFile("saved_model/cnn_sa_test.pb", "wb") as f: 
+	# with tf.gfile.GFile(filename, "wb") as f: 
 		f.write(output_graph_def.SerializeToString())
--- CNN_SA_test_restore.py
+++ CNN_SA_test_restore.py
@@ -11,17 +11,26 @@ from tensorflow.examples.tutorials.mnist import input_data
 
 model_filename_before_quantization = "saved_model/cnn_sa_test.pb";
 model_filename_after_quantization = "quantization_model/cnn_sa_test_quantizate.pb";
-model_filename = model_filename_before_quantization;
+model_filename = model_filename_after_quantization;
+
+# input  [batch, in_height, in_width, in_channels]
+# filter [filter_height, filter_width, in_channels, out_channels]
+filter_width = 3
+filter_height = filter_width
+feature_map_height = 10
+feature_map_width = 10
+input_channels = 16
+output_channels = 16
 
 # print one 'variable', but now it is constant
-with tf.Session() as sess:
-    with open(model_filename, 'rb') as f:
-        graph_def = tf.GraphDef()
-        graph_def.ParseFromString(f.read()) 
-        output = tf.import_graph_def(graph_def, return_elements=['W1:0'])
-        print(sess.run(output)) 
+# with tf.Session() as sess:
+#     with open(model_filename, 'rb') as f:
+#         graph_def = tf.GraphDef()
+#         graph_def.ParseFromString(f.read()) 
+#         output = tf.import_graph_def(graph_def, return_elements=['W1:0'])
+#         print(sess.run(output)) 
 
-# print operation name and operation type
+# print operation name and operation type, print all variable and other result
 # with open(model_filename, 'rb') as f:
 #     graph_def = tf.GraphDef()
 #     graph_def.ParseFromString(f.read())
@@ -47,34 +56,35 @@ with tf.Session() as sess:
 #                                       dropout: 1.}))
 
 # use quantized mode or unquantized mode to do inference work
-# node_name = [	'reshape_X_eightbit_reshape_X:0',
-# 				'reshape_X_eightbit_min_X:0','reshape_X_eightbit_max_X:0',
-# 				'reshape_X_eightbit_quantize_X:0','reshape_X_eightbit_quantized_reshape:0',
-# 				'W1_quint8_const:0','W1_min:0','W1_max:0','first_conv_eightbit_quantized_conv:0',
-# 				'first_conv_eightbit_requant_range:0','first_conv_eightbit_requantize:0',
-# 				'B1_quint8_const:0','B1_min:0','B1_max:0','first_bias_eightbit_quantized_bias_add:0',
-# 				'first_bias_eightbit_requant_range:0','first_bias_eightbit_requantize:0',
-# 				'Y1_eightbit_quantized:0','Y1:0']
-# mnist = input_data.read_data_sets('MNIST_data', one_hot=True, reshape=True)
-# X = tf.placeholder(tf.float32, [None, 25], name="X")
-# with tf.Session() as sess:
-#     with open(model_filename, 'rb') as f: 
-#         graph_def = tf.GraphDef()
-#         graph_def.ParseFromString(f.read()) 
-#         output = tf.import_graph_def(graph_def, input_map={'X:0': X}, 
-# 														return_elements=node_name) 
-#         batch_X = np.array([[0.8,1.,1.,1.,1.,0.,0.,0.1,1.,0.05,0.,0.,0.95,0.,0.,0.,1.,0.,0.,0.,0.85,0.,0.,0.,0.]])
-#         output_list = sess.run(output, feed_dict={X: batch_X})
-# print('X:0')
-# print(batch_X.shape)
-# print(batch_X.dtype)
-# np.savetxt('model_parameter/cnn_sa_test_V1/X:0', batch_X, fmt='%-14.4f')
-# for op in zip(output_list,node_name):
-# 	print('')
-# 	print(op[1])
-# 	print(op[0].shape)
-# 	data_type = op[0].dtype
-# 	print(data_type)
-# 	file_path = 'model_parameter/cnn_sa_test_V1/'+op[1]
-# 	data = op[0].reshape((1,-1))
-# 	np.savetxt(file_path, data, fmt='%-14.4f')
+node_name = [	'reshape_X_eightbit_reshape_X:0',
+				'reshape_X_eightbit_min_X:0','reshape_X_eightbit_max_X:0',
+				'reshape_X_eightbit_quantize_X:0','reshape_X_eightbit_quantized_reshape:0',
+				'W1_quint8_const:0','W1_min:0','W1_max:0','first_conv_eightbit_quantized_conv:0',
+				'first_conv_eightbit_requant_range:0','first_conv_eightbit_requantize:0',
+				'B1_quint8_const:0','B1_min:0','B1_max:0','first_bias_eightbit_quantized_bias_add:0',
+				'first_bias_eightbit_requant_range:0','first_bias_eightbit_requantize:0',
+				'Y1_eightbit_quantized:0','Y1:0']
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True, reshape=True)
+X = tf.placeholder(tf.float32, [None, feature_map_height*feature_map_width*input_channels], name="X")
+with tf.Session() as sess:
+    with open(model_filename, 'rb') as f: 
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read()) 
+        output = tf.import_graph_def(graph_def, input_map={'X:0': X}, 
+														return_elements=node_name) 
+        # batch_X = np.array([[0.8,1.,1.,1.,1.,0.,0.,0.1,1.,0.05,0.,0.,0.95,0.,0.,0.,1.,0.,0.,0.,0.85,0.,0.,0.,0.]])
+        batch_X = np.random.random_sample((1,feature_map_height*feature_map_width*input_channels))
+        output_list = sess.run(output, feed_dict={X: batch_X})
+print('X:0')
+print(batch_X.shape)
+print(batch_X.dtype)
+np.savetxt('model_parameter/cnn_sa_test_V1/X:0', batch_X, fmt='%-14.4f')
+for op in zip(output_list,node_name):
+	print('')
+	print(op[1])
+	print(op[0].shape)
+	data_type = op[0].dtype
+	print(data_type)
+	file_path = 'model_parameter/cnn_sa_test_V1/'+op[1]
+	data = op[0].reshape((1,-1))
+	np.savetxt(file_path, data, fmt='%-14.4f')
